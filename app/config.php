dea2eb892da8c46e789f9ade24aafe9414893d36
Rudolfo <rudolfotorres@gmail.com>
2025-08-12 22:08:17 -0300

    ajuste config

--- app/config.php
+++ app/config.php
@@ -4,7 +4,6 @@ try {
     $pdo = new PDO('sqlite:' . __DIR__ . '/projetoUCA.db');
     // Configura o PDO para lançar exceções em caso de erro
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "✅ Conexão bem-sucedida!";
 } catch (PDOException $e) {
     echo "❌ Erro na conexão: " . $e->getMessage();
 }
