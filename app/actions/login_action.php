c56113f6f42cae5530dcf5e89f63bf212e9cf338
Rudolfo <rudolfotorres@gmail.com>
2025-08-12 21:59:59 -0300

    DIR add

--- app/actions/login_action.php
+++ app/actions/login_action.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-require_once '../config.php';
+require_once __DIR__ . '/../config.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $usuario = $_POST['usuario'] ?? '';
