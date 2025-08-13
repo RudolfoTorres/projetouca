419dc5e46372c136a2adc1b45499672b085e3af4
Rudolfo <rudolfotorres@gmail.com>
2025-08-12 21:15:01 -0300

    router teste

--- public/index.php
+++ public/index.php
@@ -1,10 +1,5 @@
 <?php
-session_start();
-
-// Supondo que a variável 'loggedin' é definida na sessão após o login
-if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === true) {
-    // Se o usuário está logado, redirecione para o dashboard
-    header("Location: login.php");
-    exit;
-}
+// Redireciona o usuário para a tela de login
+header("Location: login.php");
+exit;
 ?>
